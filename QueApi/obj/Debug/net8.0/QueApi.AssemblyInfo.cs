c124651bc6f88fe781d2cf36ea28f751e9f01b90
MattiaValerio <mattia.valerio2002@gmail.com>
2024-09-03 22:58:18 +0200

    - removed projects - added websocket channel to api

--- QueApi/obj/Debug/net8.0/QueApi.AssemblyInfo.cs
+++ QueApi/obj/Debug/net8.0/QueApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+023f9075f56562af60f84ef6dd368b53a0d8aeba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc740fb2e7e854dd392d5b737c958f2460548d7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
